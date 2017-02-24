581f40593516c11888f3dcb3be54df65ca598fcc
yong <yong@estsoft.com>
2017-02-24 16:09:29 +0900

    일단 오늘까지 한거 업데이트 회원가입 html, jquery 유효성 검사까지 완료 이제 db연동하면 됨

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var securityCenter = require('./routes/securityCenter');
 var support = require('./routes/support');
