c701df454243315fac381852b0c8d9909288a04b
yongNoteBook <choyongjai@naver.com>
2017-02-26 13:05:27 +0900

    DB커넥트 성공

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.engine('html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//페이지 연결
+app.get('/', function(req, res) {
+    res.render('join');
+});
+
+//DB 커넥트
+mongoose.connect("mongodb://yong:171225@ds145389.mlab.com:45389/survival");
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
+
+
+
+app.listen(3000);
+console.log("Server running on port 3000");
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.engine('html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('join');
+});
+
+mongoose.connect("mongodb://yong:171225@ds145389.mlab.com:45389/survival");
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
