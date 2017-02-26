954791f2e2c2cf371789f27d2211e324d9436982
yongNoteBook <choyongjai@naver.com>
2017-02-26 20:24:04 +0900

    커밋

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ app.set('view engine', 'ejs');
 
 //페이지 연결
 app.get('/', function(req, res) {
+    res.render('main');
+});
+app.get('/join', function(req, res) {
     res.render('join');
 });
+
+
+app.post('/join', function(req, res) {
+    res.json(req.body);
+});
+
 
 //DB 커넥트
 mongoose.connect("mongodb://yong:171225@ds145389.mlab.com:45389/survival");
@@ -24,6 +33,20 @@ db.on("error",function (err) {
   console.log("DB ERROR :", err);
 });
 
+
+
+// //회원 DB
+// var userData = mongoose.Schema({
+// 	userId:String,
+// 	userPw:String,
+// 	userNick:String
+// });
+
+// var userData = mongoose.model("userData", userData);
+// var userDataIns = new userData({userId: "qwe",userPw: "123",userNick: "asd"});
+// userDataIns.save(function(err, userDataIns){
+	
+// });
 
 
 app.listen(3000);
